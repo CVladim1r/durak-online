291c36587148b49e467284ce3a3a3e6423a094ff
CVladim1r <vladimir.973@list.ru>
2024-07-10 04:09:03 +0300

    w-v-2

--- durak-app/src/pages/ProfilePage.tsx
+++ durak-app/src/pages/ProfilePage.tsx
@@ -1,32 +1,25 @@
-import React, { useEffect, useState } from 'react';
-import '../pages/_profilePage.scss';
+import { useEffect, useState } from 'react';
 
 const ProfilePage: React.FC = () => {
-  const [userData, setUserData] = useState<any>({});
+    const [userData, setUserData] = useState<any>(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        await window.Telegram.WebApp.init();
-        const userData = window.Telegram.WebApp.initDataUnsafe.user;
-        setUserData(userData);
-      } catch (error) {
-        console.error('Error initializing Telegram WebApp:', error);
-      }
-    };
+    useEffect(() => {
+        const Telegram = (window as any).Telegram;
+        if (Telegram && Telegram.WebApp && Telegram.WebApp.initDataUnsafe) {
+            const userData = Telegram.WebApp.initDataUnsafe.user;
+            setUserData(userData);
+        }
+    }, []);
 
-    fetchData();
-  }, []);
-
-  return (
-    <div className="user-info">
-      <h2>User Profile</h2>
-      <p><strong>Name:</strong> {userData.first_name} {userData.last_name}</p>
-      <p><strong>Username:</strong> @{userData.username}</p>
-      <p><strong>Language Code:</strong> {userData.language_code}</p>
-      <p><strong>User ID:</strong> {userData.id}</p>
-    </div>
-  );
-};
+    return (
+        <div className="user-info">
+            <h2>User Profile</h2>
+            <p><strong>Name:</strong> {userData?.first_name} {userData?.last_name}</p>
+            <p><strong>Username:</strong> @{userData?.username}</p>
+            <p><strong>Language Code:</strong> {userData?.language_code}</p>
+            <p><strong>User ID:</strong> {userData?.id}</p>
+        </div>
+    );
+}
 
 export default ProfilePage;
